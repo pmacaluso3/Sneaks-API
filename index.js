e38f19035a06d2dbc7e05d08f8e403f00c692e63
famousPete <pete.macaluso@generalassemb.ly>
2020-12-04 17:10:57 -0500

    do not run webserver

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ mongoose.Promise = global.Promise;
 // Connecting to the database
 //mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/sneakers');
 
-app.listen(port, function () {
-  console.log(`Sneaks app listening on port `, port);
- });
+// app.listen(port, function () {
+//   console.log(`Sneaks app listening on port `, port);
+//  });
 
 module.exports = app;
 module.exports = SneaksAPI;
